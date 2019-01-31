ebee371b67170f61300083f1b909fb819d706f1e
Anna Senchukova <asenchukova@gmail.com>
2019-01-31 11:30:00 +0300

    2

--- cyt3.py
+++ cyt3.py
@@ -1,8 +1,10 @@
 from Bio import Phylo 
 import pylab 
-f = 'path/to/my/file' 
-tree = Phylo.read('RAxML_bipartitionsBranchLabels.T20', 'newick') 
-tree.ladderize() 
-Phylo.draw(tree, do_show=False) 
-pylab.axis() 
-pylab.savefig("tree2.png",format='png', bbox_inches='tight', dpi=300) 
+def draw_tree(path_to_file):
+	tree = Phylo.read('RAxML_bipartitionsBranchLabels.T20', 'newick') 
+	tree.ladderize() 
+
+	Phylo.draw(tree, label_func= str, show_confidence = True, do_show=False)
+	pylab.axis() 
+	pylab.savefig("tree2.png",format='png', bbox_inches='tight', dpi=300) 
+draw_tree('tree2')
