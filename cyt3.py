fcc53919ecd44ef52aaa775d6dcf4f743d3604d9
Anna Senchukova <asenchukova@gmail.com>
2019-02-01 12:28:54 +0300

    4

--- cyt3.py
+++ cyt3.py
@@ -2,12 +2,13 @@ from Bio import Phylo
 import pylab 
 import sys
 print(sys.argv)
-
+in_tree = sys.argv[1]
+out_tree = sys.argv[2]
 def draw_tree(path_to_file):
-	tree = Phylo.read(sys.argv[1], 'newick') 
+	tree = Phylo.read(in_tree, 'newick') 
 	tree.ladderize() 
 
 	Phylo.draw(tree, label_func= str, show_confidence = True, do_show=False)
 	pylab.axis() 
-	pylab.savefig("tree2.png",format='png', bbox_inches='tight', dpi=300) 
-draw_tree('tree2')
+	pylab.savefig(out_tree,format='png', bbox_inches='tight', dpi=300) 
+draw_tree(out_tree)
